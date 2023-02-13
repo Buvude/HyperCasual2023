71dc56d7e11b4fa6a9cda77e4ea9959e3eb0d4de
Buvude <90856761+Buvude@users.noreply.github.com>
2023-02-13 12:48:21 -0500

    got basic spawner working

--- Hypercasual/Assets/Scripts/ObjectSpawning.cs
+++ Hypercasual/Assets/Scripts/ObjectSpawning.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObjectSpawning : MonoBehaviour
 {
+    public List<GameObject> ObjectsSpawned = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
